d4b4a4c52e1bf0880871cc8946fdd6809dda92f7
faraneh <faranehyarahmadi@gmail.com>
2024-05-15 19:00:55 +0330

    add menu and userprofie

--- devshop/core/views.py
+++ devshop/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from store.models import Product , Category
 # Create your views here.
+
+def frontpage (request):
+    products = Product.objects.all()[:6]
+    categories=Category.objects.all()[:6]
+    return render(request, 'core/frontpage.html', {
+        'products' : products ,
+        'categories' : categories
+    })
+
+
+def about(request):
+    return render(request, 'core/about.html')
